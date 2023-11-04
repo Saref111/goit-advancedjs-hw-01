e4284b789dc09900a0a4f68396ebafd8a7e87b56
Saref111 <50395293+Saref111@users.noreply.github.com>
2023-11-04 09:53:46 +0000

    Deploying to gh-pages from @ Saref111/goit-advancedjs-hw-01@a787e42f8565a19ec430fc11b14c7145f8c772d5 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const i=document.querySelector("iframe"),t=new r(i),e=localStorage.getItem("videoplayer-current-time"),m=a(function(o){localStorage.setItem("videoplayer-current-time",o.seconds)},1e3);e&&t.setCurrentTime(e);t.on("timeupdate",m);
+//# sourceMappingURL=commonHelpers2.js.map
