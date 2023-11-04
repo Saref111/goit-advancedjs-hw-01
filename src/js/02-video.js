a787e42f8565a19ec430fc11b14c7145f8c772d5
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-04 11:53:26 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const prevTime = localStorage.getItem('videoplayer-current-time');
+const onTimeUpdate = throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+if (prevTime) {
+    player.setCurrentTime(prevTime);
+}
+
+player.on('timeupdate', onTimeUpdate);
