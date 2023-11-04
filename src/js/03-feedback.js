b48ef38014ff9ba151d40163f436f464bcedf8fa
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-04 12:02:42 +0200

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
 
+const feedbackForm = document.querySelector('.feedback-form');
+const onInput = (e) => {
+    const feedback = {};
+    const formData = new FormData(feedbackForm);
+    formData.forEach((value, name) => {
+        feedback[name] = value;
+    });
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+};
+const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.removeItem('feedback-form-state');
+    e.currentTarget.reset();
+};
+
+feedbackForm.addEventListener('input', onInput);
+feedbackForm.addEventListener('submit', onSubmit);
+
+const feedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (feedback) {
+    const { email, message } = feedback;
+    feedbackForm.elements.email.value = email;
+    feedbackForm.elements.message.value = message;
+}
